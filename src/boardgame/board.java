b34b567290097162ce7ea940bad702b80a0266e5
Vinicius Trigo <viniciustrigo52@gmail.com>
2021-01-12 10:12:24 -0300

    BoardException

--- src/boardgame/board.java
+++ src/boardgame/board.java
@@ -7,6 +7,9 @@ public class board {
 	private piece[][] pieces;
 	
 	public board(Integer rows, Integer columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new piece[rows][columns];
@@ -16,28 +19,44 @@ public class board {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-	
 	public piece piec(Integer rows, Integer column) {
+		if (!positionExist(rows, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[rows][column];
 	}
 	
 	public piece piece(position p) {
+		if (!positionExist(p	)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[p.getRow()][p.getColunm()];
 	}
 	
 	public void placePiece(piece pie, position ps) {
+		if (thereisAPice(ps)) {
+			throw new BoardException("There is already a piece on position" +ps);
+		}
 		pieces[ps.getRow()][ps.getColunm()]=pie;
 		pie.position = ps;
+	}
+	
+	private boolean positionExist(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExist(position ps) {
+		return positionExist(ps.getRow(), ps.getColunm());
+	}
+	
+	public boolean thereisAPice(position p) {
+		if (!positionExist(p)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(p) != null;
 	}
 }
