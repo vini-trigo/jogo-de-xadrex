9f72f567fa216c818475fe783cc883a56e3dc747
Vinicius Trigo <viniciustrigo52@gmail.com>
2021-01-14 00:23:07 -0300

    Pawn

---
+++ src/chess/pieces/copy/Pawn.java
@@ -0,0 +1,66 @@
+package chess.pieces.copy;
+
+import boardgame.position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece{
+
+	public Pawn(boardgame.board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		position p = new position(0, 0);
+
+		if(getColor() == Color.WHITE) {
+			p.setValues(position.getRow() -1, position.getColunm());
+			if(getBoard().positionExist(p) && !getBoard().thereisAPice(p)) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+			p.setValues(position.getRow() -2, position.getColunm());
+			position p2 = new position(position.getRow() -1, position.getColunm());
+			if(getBoard().positionExist(p) && !getBoard().thereisAPice(p) && getBoard().positionExist(p2) && !getBoard().thereisAPice(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+			p.setValues(position.getRow() -1, position.getColunm()-1);
+			if(getBoard().positionExist(p) && isThereAnyPossibleMove(p)) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+			p.setValues(position.getRow() -1, position.getColunm()+1);
+			if(getBoard().positionExist(p) && isThereAnyPossibleMove(p)) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+		}
+		else {
+			p.setValues(position.getRow() +1, position.getColunm());
+			if(getBoard().positionExist(p) && !getBoard().thereisAPice(p)) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+			p.setValues(position.getRow() +2, position.getColunm());
+			position p2 = new position(position.getRow() -1, position.getColunm());
+			if(getBoard().positionExist(p) && !getBoard().thereisAPice(p) && getBoard().positionExist(p2) && !getBoard().thereisAPice(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+			p.setValues(position.getRow() +1, position.getColunm()-1);
+			if(getBoard().positionExist(p) && isThereAnyPossibleMove(p)) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+			p.setValues(position.getRow() +1, position.getColunm()+1);
+			if(getBoard().positionExist(p) && isThereAnyPossibleMove(p)) {
+				mat[p.getRow()][p.getColunm()] = true;
+			}
+		}
+		
+		return mat;
+	}
+
+	@Override
+	public String toString() {
+		return "P";
+	}
+
+	
+}
